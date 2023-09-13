6f259aee3f71539355b261bf99de43c5f417a817
CrombachNatasja <463130@student.fontys.nl>
2023-09-13 15:19:40 +0200

    joejoe

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,19 @@
 print("hello world")
 print("hoihoi")
+
+def number(a: int) -> int :
+    """"Dit is een korte uitleg over de functie, dit MOET op het tentamen.
+    :param a: The number
+    :return: a:"""
+    return a
+
+# Werk met functies in plaats van één lap tekst.
+# Alle losse onderdelen zijn een functie, zie slides DS5 week 2
+# Maak met je groepje een overzicht van de codes, welke functies hebben we nodig?,
+# daarna taakverdeling functies schrijven en ieder aan een eigen functie werken.
+
+# Main functie: je runt de folder of je navigeert naar de main functie
+def main(n_games:int) -> list:
+    """Main functie roept de andere functies een voor een aan, hij runt in 1x heel je programma.
+    Je kunt maar 1 main functie maken.
+    from GoodCode_example import main"""
